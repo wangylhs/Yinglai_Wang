e29b9abe0ed1f82b1bf082b98b8a8a08caf793d5
wang1105 <wang1105@purdue.edu>
2017-06-14 15:19:28 -0400

    d14

--- MyPlayground_1.playground/Contents.swift
+++ MyPlayground_1.playground/Contents.swift
@@ -15,8 +15,7 @@ let len = 90
 let len_label = label + String(len)
 
 let first: float_t = 1.5
-let greeting = "I have \(float_const + first) friends"
-print(greeting)
+//let greeting = "I have \(float_const + first) friends"
 
 // swift 4 supports 3 quotes
 /*let sentences = """
@@ -37,4 +36,68 @@ var occupations = [
         "Ted": "Mechanic"
 ]
 print(occupations)
+//create empty array or dictionary
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+occupations = [:]
+print(occupations)
+
+let individualScores = [1,2,3,4,5]
+var teamScore = 0
+
+for score in individualScores{
+    if score > 3 {
+        teamScore += score
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = nil
+var greeting = "Hello"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+} else {
+    greeting = "Hello, \"nil\""
+}
+print(greeting)
+
+let nickName: String? = "John"
+let fullName: String = "Johnathon Smith"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+let vegetable = "red onion"
+switch vegetable {
+case "carrot":
+    print("carrot............")
+case "onion","potato":
+    print("Stew with meat would be good")
+case let x where x.hasSuffix("pepper"):
+    print("Is \(x) spicy?")
+default:
+    print("everything tastes good in soup.")
+}
+
+var numDic = [
+    "Prime": [2,3,5,7,11,13,17],
+    "Fibonacci": [1,1,2,3,5,8,13],
+    "Square":[1,4,9,16,25,36],
+]
+var largest = 0
+var largestKind: String = "NULL"
+for (kind, numbers) in numDic{
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestKind = kind
+        }
+    }
+}
+//numDic["largest"] = [largest]
+print("largestKind is \(largestKind) and largest number is \(largest)")
+
 
