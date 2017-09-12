bc116a4031960a52f8486efb79bfd8ea7984c302
wang1105 <wang1105@purdue.edu>
2017-09-12 16:43:52 -0400

    d17

---
+++ Practice/src/StringMani.java
@@ -0,0 +1,21 @@
+
+public class StringMani {
+	
+	//determine if a string has all unique characters
+	public static boolean isUnique(String s){
+		if(s.length()>128){
+			return false;
+		}
+		boolean[] b = new boolean[128];
+		for(int i=0;i<s.length();i++){
+			
+		}
+		
+	}
+	
+	
+	
+	public static void main(String[] args){
+		
+	}
+}
